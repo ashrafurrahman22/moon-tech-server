b1590f39a56e037ad5ee3ab44e740086e3479e0d
ASHRAFUR RAHMAN <you@example.com>
2023-02-24 14:26:48 +0600

    updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -9,12 +10,13 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.us3tiud.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
   try {
@@ -28,6 +30,14 @@ const run = async () => {
       res.send({ status: true, data: product });
     });
 
+     // catch single item
+     app.get('/products/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const product = await productCollection.findOne(query);
+      res.send(product);
+  })
+
     app.post("/product", async (req, res) => {
       const product = req.body;
 
@@ -35,6 +45,31 @@ const run = async () => {
 
       res.send(result);
     });
+
+
+
+    // update route
+    app.put('/products/:id', async(req, res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const updatedProducts = req.body;
+      console.log(updatedProducts);
+      const filter = {_id : ObjectId(id)};
+      const options = { upsert : true };
+      const updatedDoc = {
+          $set : {
+              model : updatedProducts.model,
+              brand: updatedProducts.brand,
+              status : updatedProducts.status,
+          }
+      };
+      const result = await productCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+      })
+
+
+
+
 
     app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
